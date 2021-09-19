0034dfc93a015d038b7e19dac212913352b4d5e3
Eduardo Assis <84932877+Ducabelo@users.noreply.github.com>
2021-09-19 14:16:26 -0300

    Update counter.js

--- counter/counter.js
+++ counter/counter.js
@@ -11,16 +11,12 @@ increase.addEventListener("click", increaseNumber);
 
 function decreaseNumber(number){
 
-  number = 1;
-
   document.getElementById('number').textContent -= initialValue + 1;
   
 }
 
 
 function increaseNumber(){
-
-  number = 1;
 
   document.getElementById('number').textContent -= initialValue - 1;
 
